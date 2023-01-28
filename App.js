34107c99f01543f74627cf312b4bb557d8bda0c7
Mihir <mihirpoojara1310@gmail.com>
2023-01-28 12:28:55 +0530

    Lecture - 3

--- App.js
+++ App.js
@@ -1,43 +1,15 @@
-/**
- *
- * HMR : Hot Module Replacement
- * File Watcher Algorithms - C++
- * Building
- * Minification
- * Cleaning the code
- * Dev and production build
- * Super Fast build algorithm
- * Image Optimisation
- * Caching while development
- * Compression
- * Compatible with older versions of browsers
- * It adds polyfills
- * HTTPS on dev
- * Port numbers
- * Consistent Hashing Algorithms
- * Zero Config Bundlers
- *
- */
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const headingForReact1 = React.createElement(
-  "h1",
-  { id: "title", key: "1" },
-  "Heading from Parcel"
-);
-const headingForReact2 = React.createElement(
-  "h2",
-  { id: "title", key: "2" },
-  "Heading 2"
-);
-const container = React.createElement("div", { id: "container" }, [
-  headingForReact1,
-  headingForReact2,
-]);
+const Component = () => <h3>React Component</h3>;
 
-const rootForReact = ReactDOM.createRoot(
-  document.getElementById("rootForReact")
+const FunctionalComponent = () => (
+  <div>
+    <h1>Namaste</h1>
+    <h2>React Functional Component</h2>
+    <Component />
+  </div>
 );
-rootForReact.render(container);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<FunctionalComponent />);
